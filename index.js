409611726a205607469e2a740bab58b54a7fc719
chefo11 <fran6cool@gmail.com>
2022-04-27 05:00:36 +0100

    working on responsiveness

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //sidebar
 const menuItems = document.querySelectorAll(".menu-item");
+//messages
+const messagesNotification = document.querySelector("#messages-notifications");
+const messages = document.querySelector(".messages");
 
 menuItems.forEach((item) => {
   item.addEventListener("click", () => {
@@ -14,6 +17,18 @@ menuItems.forEach((item) => {
       document.querySelector(".notification-popup").style.display = "none";
     } else {
       document.querySelector(".notification-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
     }
   });
+});
+//messages
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
 });
